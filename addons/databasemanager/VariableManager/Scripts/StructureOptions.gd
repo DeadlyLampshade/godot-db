893ce1b10e6d6fdf179e465d9f486eb2b0e33f67
DeadlyLampshade <jordanlucas01@gmail.com>
2017-12-20 18:05:58 +1000

    Added a temporary means to visualize PanelContainers

--- addons/databasemanager/VariableManager/Scripts/StructureOptions.gd
+++ addons/databasemanager/VariableManager/Scripts/StructureOptions.gd
@@ -8,3 +8,8 @@ func unclean(data):
 	if data != null:
 		$VBoxContainer/HBoxContainer/RowCount.value = data.row_count
 		$VBoxContainer/MainContainer.unclean(data.contents)
+
+func click_hide_button():
+	$VBoxContainer/MainContainer.visible = !$VBoxContainer/MainContainer.visible
+	$VBoxContainer/HBoxContainer/Button.text = "Show" if $VBoxContainer/MainContainer.visible else "Hide"
+	pass # replace with function body
